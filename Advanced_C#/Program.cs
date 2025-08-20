4dd44f3d5a3d9c41a959309ee3dd0d637184c95e
Nancy <ny6049812@gmail.com>
2025-08-20 11:17:35 +0300

    Q1

--- Advanced_C#/Program.cs
+++ Advanced_C#/Program.cs
@@ -3,10 +3,60 @@
 namespace MyApp
 {
     internal class Program
+
     {
+
+        #region Q1
+        static void OptimizedBubbleSort(int[] arr)
+        {
+
+            int n = arr.Length;
+            bool swapped;
+            for (int i = 0; i < n - 1; i++)
+            {
+                swapped = false;
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+                if (!swapped)
+                    break;
+            }
+            {
+
+            }
+
+
+
+        }
+
+
+
+
+
+        #endregion
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+             
+            #region Q1
+            int[] numbers=
+                { 64, 34, 25, 12, 22, 11, 90 };
+
+            Console.WriteLine("Before Sorting: " + string.Join(", ", numbers));
+            OptimizedBubbleSort(numbers);
+            Console.WriteLine("After Sorting:  " + string.Join(", ", numbers));
+
+            #endregion
+
+
+
+
         }
     }
 }
